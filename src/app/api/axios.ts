8a72043002adee55744542715d3eedc61bc656ac
JEunseo <jjeseo1010@gmail.com>
2024-11-05 16:24:47 +0900

    vercelFront + release0.08

--- src/app/api/axios.ts
+++ src/app/api/axios.ts
@@ -3,11 +3,11 @@
 import axios from "axios";
 
 export const instance  = axios.create ({
-    baseURL : process.env.NEXT_PUBLIC_REACT_APP_SERVER_URL
+    baseURL : process.env.NEXT_PUBLIC_REACT_APP_SERVER_URL || "http://localhost:8080", 
 }); 
 
 export const instance1  = axios.create ({
-    baseURL : process.env.NEXT_PUBLIC_REACT_APP_SERVER_URL
+    baseURL : process.env.NEXT_PUBLIC_REACT_APP_SERVER_URL || "http://localhost:8081", 
 }); 
 
 export default instance;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,29 @@
 import './globals.css';
 import StoreProvider from './StoreProvider';
 import { useSearchContext } from './components/SearchContext';
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import TabFeatures from './(page)/restaurant/page';
 import { useRouter } from 'next/navigation';
 import Home from './(page)/home/page';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import TodayPost from './(page)/post/today/page';
 
 const Page = () => {
     const { searchTerm } = useSearchContext();
     const [isTabVisible, setIsTabVisible] = useState(true);
     const router = useRouter();
 
+    const queryClient = new QueryClient({
+        defaultOptions: {
+            queries: {
+                refetchOnWindowFocus: true, 
+                staleTime: 0, 
+            },
+        },
+    });
+
     useEffect(() => {
+        // 페이지 로드 시 searchTerm에 따라 초기 상태 설정
         if (searchTerm) {
             setIsTabVisible(false);
             router.push('/home'); // 검색어가 있을 때 Home으로 리디렉션
@@ -20,12 +32,14 @@ const Page = () => {
             setIsTabVisible(true);
             router.push('/'); // 검색어가 없을 때 TabFeatures로 이동
         }
-    }, [searchTerm]);
+    }, [searchTerm, router]);
 
     return (
-        <StoreProvider>
-            {isTabVisible ? <TabFeatures start={0} limit={10} /> : <Home />}
-        </StoreProvider>
+        <QueryClientProvider client={queryClient}>
+            <StoreProvider>
+                {isTabVisible ? <TabFeatures start={0} limit={10} /> : <Home />}
+            </StoreProvider>
+        </QueryClientProvider>
     );
 };
 
