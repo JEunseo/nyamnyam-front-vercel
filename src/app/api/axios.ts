afec5a4de70958a2a39ee6c19f2b423746dd188f
JEunseo <jjeseo1010@gmail.com>
2024-11-05 17:41:39 +0900

    8000 delete

--- src/app/api/axios.ts
+++ src/app/api/axios.ts
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 export const instance  = axios.create ({
-    baseURL : process.env.NEXT_PUBLIC_REACT_APP_SERVER_URL || "http://localhost:8000", 
+    baseURL : process.env.NEXT_PUBLIC_REACT_APP_SERVER_URL
 }); 
 
 export const instance1  = axios.create ({
